a62ea548e28b2ac510bbbc7e6fa0e7bf8a1b1feb
vika <vika.sokolova728@gmail.com>
2021-04-02 14:54:25 +0600

    last commit

---
+++ product/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Product
+
+
+class CreateProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class UpdateProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
